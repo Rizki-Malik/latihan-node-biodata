cd838f00a09288c2bd7549c30a0d122e33ffcea3
DariusTorto <rizkimalik221001@gmail.com>
2023-05-09 21:21:17 +0700

    first

---
+++ moduleBiodata.js
@@ -0,0 +1,11 @@
+let biodata = {
+    nama: 'Maman Budiman',
+    tempatLahir: 'Cianjur',
+    tanggalLahir: '8 Agustus 2000',
+    alamat: 'Jl. Neptunus No. 90'
+  };
+  
+  console.log(`Nama: ${biodata.nama}`);
+  console.log(`Tempat Lahir: ${biodata.tempatLahir}`);
+  console.log(`Tanggal Lahir: ${biodata.tanggalLahir}`);
+  console.log(`Alamat: ${biodata.alamat}`);
